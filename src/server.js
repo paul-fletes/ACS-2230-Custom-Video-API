259a99b902e0a6e724586a0749c348af1a9cd0a6
Paul Fletes <paul.fletes@gmail.com>
2023-11-08 15:19:03 -0600

    add server

---
+++ src/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+require('./data/db');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+require('./controllers/auth')(app);
+require('./controllers/video')(app);
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+module.exports = app;
